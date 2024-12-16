e17ed299a90ccb6e1d83b9e839aa095f51853862
Ishaawn <2020.ishan.paul@gmail.com>
2024-12-16 17:48:27 +0000

    Initial commit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,21 @@
 class App {
   public static void main(String[] args) {
-    Book aBook = new Book();
-    System.out.println(aBook.pigLatin("hello"));
-    System.out.println(aBook.translateWord(""));
+
+
+
+
+
+ Book myBook = new Book("https://www.gutenberg.org/cache/epub/84/pg84.txt");
+ int myBookwords=myBook.wordCount();
+   System.out.println(myBook.translateWord(myBook.bookText()));
+   System.out.println("There are "+myBookwords+" words in Frankenstein, or a Modern Prometheus");
+ 
+
+   Book Sosbook = new Book("https://www.gutenberg.org/cache/epub/11/pg11.txt");
+int Sosbookwords=Sosbook.wordCount();
+ System.out.println(Sosbook.translateWord(Sosbook.bookText()));
+ System.out.println("There are "+Sosbookwords+" words in Alice's adventures in Wonderland");
+System.out.println(myBook.translateSentence("I can now speak in Pig-Latin. I can translate words and sentences for the students at Foothill High School."));
   }
 }
+
--- src/Book.java
+++ src/Book.java
@@ -1,34 +1,256 @@
-//A few assumptions.......
-
-//Words will be separated by spaces. 
-//There can be punctuation in a word, we will only add/keep punctuation at the end of a string if it is at the end of a string.
-//    for examples: Hello.==> Ellohay.    Good-bye! ==> Ood-byegay!    so... ==> osay...
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.net.URL;
 
 public class Book
 {
-  public String pigLatin(String word)
-  {
 
-  }
   
+/*private String bookText;
+public Book(String link)
+{
+readBook(link);
+}
+private void readBook(String link)
+{
+try 
+{
+URL url=new URL(link);
+Scanner s=new Scanner (url.openStream());
+
+while (s.hasNext())
+{
+String text=s.nextLine();
+bookText+=translateSentence(text);
+}
+}
+catch(IOException ex)
+{
+ex.printStackTrace();
+}
+}
+*/
+
+
+
+
+
+
   public int endPunctuation(String word)  //return the index of where the punctuation is at the end of a String. If it is all punctuation return 0, if there is no punctuation return -1
   {
 
-    return -1;
+    return word.length()-1;
+    //int words=0;
   }
+ 
+  public String pigLatin(String word)
+  {
+  String vowels="aeiou";
+  String first=word.substring(0,1);
+  String second;
+  if (word.length()==1) 
+  {
+    second="";
+  }
+else second=word.substring(1,2);
+
+  if (vowels.indexOf(first)>=0)
+  {
+     return word + "yay";
+  }
+
+  else if(vowels.indexOf(second)>=0)
+  {
+    return word.substring(1)+first+"ay";
+  }
+  else 
+  {
+    return word.substring(2)+first+second+"ay";
+  }
+
+  }
+
+
+public int puncuationalClose(String word)
+{
+
+return word.length()-1;
+}
+
+  int words=0;
 
   public String translateWord(String word)    //to share with class
   {
-    String convertedWord = "";
+    
+    String vowels = "aeiou";
+    String second ="";
+    String first="";
+    String punctuation ="!?.,;:";
+String puncuationalClose = "";
+boolean quotedTwice = false;
+boolean staringquote = false;
+boolean endquote=false;
+boolean capitals=false;
+String convertedWord2="";
+if (word==null){
+  return "";
+}
+ 
+  if(word.length()>=4){
+    if(word.substring(word.length()-3).equals("...")){
+      puncuationalClose=word.substring(word.length()-4);
+      word=word.substring(0,word.length()-4);
+    }
+    }
+  
+ 
 
-    return convertedWord;
+
+
+if(word.length()>0){
+  if(word.substring(0,1).equals("\"")&& word.substring(word.length()-1).equals("\"")&&word.length()>2){
+quotedTwice=true;
+word=word.substring(1,word.length()-2);
+  }
+if(word.substring(0,1).equals("\"")){
+staringquote=true;
+word=word.substring(0,word.length()-1);
+}
+
+if(punctuation.indexOf(word.substring(puncuationalClose(word)))>=0 && word.length()>0)
+{
+  puncuationalClose=word.substring(word.length()-1);
+  word=word.substring(0,word.length()-1);
+}
+}
+if(word.length()>0){
+  if(word.substring(0,1).equals(word.substring(0,1).toUpperCase())){
+    capitals=true;
+  }
+}
+   if (word!=""){
+    words++;
   }
 
-  public String translateSentence(String sentence)
-  {
-    String retSentence = "";
+if(capitals==true&&word.length()==1){
+  convertedWord2=pigLatin(word);
+}
+if(capitals==true&&word.length()>1){
+  convertedWord2=word.substring(1,2).toUpperCase()+word.substring(2,word.length())+word.substring(0,1).toLowerCase()+"ay";
+}
+if(capitals==false&&word.length()>0){
+  convertedWord2=pigLatin(word);
+}
+if(quotedTwice)
+{
+return "\"" + convertedWord2+puncuationalClose+"\"";
+}
+else if (endquote){
+  return convertedWord2 + puncuationalClose;
+}
+else if(staringquote){
+return "\""+convertedWord2+puncuationalClose;
 
+}
+else {
+  return convertedWord2 + puncuationalClose;
+  
+} 
+
+}
+
+/* 
+  public String translateWord2(String word){
+    String punctuation ="!?.,;:";
+String puncuationalClose ="";
+boolean quotedTwice = false;
+boolean capitals=false;
+String convertedWord2="";
+  
+if(punctuation.indexOf(word.substring(puncuationalClose(word)))>=0){
+{
+puncuationalClose=word.substring(word.length()-1);
+word=word.substring(0,word.length()-1);
+}
+*/
+  
+
+
+
+
+
+  
+
+
+ public String translateSentence(String sentence){
+  {
+int i =0;
+    String retSentence = "";
+while(sentence.indexOf(" ",i)>-1){
+
+   String word=sentence.substring(i, sentence.indexOf(" ",i));
+   i= sentence.indexOf(" ",i);
+    String result = translateWord(word);
+    i++;
+    retSentence+=result+" ";
+    
+}
+retSentence += translateWord(sentence.substring(i));
 
     return retSentence;
   }
-}  
+  }
+
+
+private String THEbook;
+public Book(String link)
+{
+  
+readBook(link);
+}
+private void readBook(String link){
+boolean read=false;
+
+try 
+{
+URL url=new URL(link);
+Scanner s=new Scanner (url.openStream());
+
+while (s.hasNext())
+{
+String text=s.nextLine();
+if(text.indexOf("*** END")>-1)read=false;
+if(read){
+text=translateSentence(text);
+THEbook+=text;
+}
+if(text.indexOf("*** START")>-1)read=true;
+
+}
+}
+catch(IOException ex)
+{
+ex.printStackTrace();
+}
+}
+
+ public String bookText(){
+    return THEbook;
+  }
+  public int wordCount(){
+    return words;
+  }
+  public void toTXT(String filename){
+try{
+String book=this.THEbook;
+PrintWriter out=new PrintWriter(filename+".txt");
+out.println(book);
+out.close();
+ }
+  catch(FileNotFoundException e){
+  System.out.println(e);
+  }
+}
+}
